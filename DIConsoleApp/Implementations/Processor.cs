52ff307701f2a48e2c12a0171c948c5a3a3d16e0
Santhosh Kumar Sriramoji <sriramojisanthoshkumar@gmail.com>
2021-07-18 17:19:15 +0530

    Commit Console Application with Dependency Injection usage.

---
+++ DIConsoleApp/Implementations/Processor.cs
@@ -0,0 +1,18 @@
+﻿using DIConsoleApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DIConsoleApp.Implementations
+{
+    public class Processor : IProcessor
+    {
+        public async Task Waiter()
+        {
+            await Task.Delay(10000);
+        }
+    }
+}
---
+++ DIConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using DIConsoleApp.Implementations;
+using DIConsoleApp.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DIConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Create a new instance of ServiceCollection.
+            var serviceCollection = new ServiceCollection();
+
+            // Configure the required services into the serviceCollection.
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            try
+            {
+                // Run the initial service.
+                await serviceProvider.GetService<IProcessor>().Waiter();
+
+                Console.WriteLine("Task Accomplished successfully!");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception to console.
+                Console.WriteLine($"Error occurred: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IProcessor, Processor>();
+        }
+    }
+}
